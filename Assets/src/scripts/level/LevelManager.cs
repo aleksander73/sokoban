cc1376786eb2d5caeaee5d4c80be58ab4b71071a
aleksander73 <olekdobkowski@gmail.com>
2023-10-21 21:20:11 +0200

    Introduce clearing of levels

--- Assets/src/scripts/level/LevelManager.cs
+++ Assets/src/scripts/level/LevelManager.cs
@@ -16,10 +16,6 @@ public class LevelManager : MonoBehaviour {
 	private Grid<Cell> grid;
 
 	public void LoadLevel(int levelIndex) {
-		this.ClearLevel();
-
-		// --------------------------------------------------
-
 		LevelReaderInput levelReaderInput = new LevelReaderInput(
 			this.floor,
 			this.target,
@@ -32,37 +28,47 @@ public class LevelManager : MonoBehaviour {
 
 		// --------------------------------------------------
 
+		GameObject levelRoot = new GameObject("level");
+
 		// Instantiate grid
 		Grid<Cell> grid = level.GetGrid();
-		this.InstantiateGrid(grid);
+		this.InstantiateGrid(levelRoot, grid);
 
 		// Instantiate player
 		Vector3 playerPosition = new Vector3(level.GetPlayer().x, level.GetPlayer().y, PLAYER_LEVEL);
-		GameObject instance = GameObject.Instantiate(this.player, playerPosition, Quaternion.identity);
+		GameObject instance = GameObject.Instantiate(this.player, playerPosition, Quaternion.identity, levelRoot.transform);
 		instance.name = Utility.SimplifyInstanceName(instance.name);
 
 		// Instantiate boxes
 		List<Vector2> boxes = level.GetBoxes();
 		boxes.ForEach(box => {
 			Vector3 boxPosition = new Vector3(box.x, box.y, PLAYER_LEVEL);
-			instance = GameObject.Instantiate(this.box, boxPosition, Quaternion.identity);
+			instance = GameObject.Instantiate(this.box, boxPosition, Quaternion.identity, levelRoot.transform);
 			instance.name = Utility.SimplifyInstanceName(instance.name);
 		});
 	}
 
-	private void ClearLevel() {
+	public void ClearLevel() {
+		GameObject level = GameObject.Find("level");
+		if(level == null) {
+			return;
+		}
 
+		GameObject camera = GameObject.Find("camera");
+		camera.transform.SetParent(null);
+		camera.transform.position = new Vector3();
+
+		GameObject.Destroy(level);
 	}
 
-	private void InstantiateGrid(Grid<Cell> grid) {
-		GameObject level = new GameObject("level");
+	private void InstantiateGrid(GameObject levelRoot, Grid<Cell> grid) {
 		List<Node<Cell>> nodes = grid.GetNodes();
 		for(int i = 0; i < nodes.Count; i++) {
 			Node<Cell> node = nodes[i];
 			GameObject sprite = node.GetValue().GetSprite();
 
 			Vector3 position = new Vector3(node.GetPosition().x, node.GetPosition().y, CELL_LEVEL);
-			GameObject instance = GameObject.Instantiate(sprite, position, Quaternion.identity, level.transform);
+			GameObject instance = GameObject.Instantiate(sprite, position, Quaternion.identity, levelRoot.transform);
 			instance.name = Utility.SimplifyInstanceName(instance.name);
 		}
 	}
