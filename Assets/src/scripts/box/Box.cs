8a15d0e59f92f0889d875bed497868b0c759231d
aleksander73 <olekdobkowski@gmail.com>
2024-06-09 08:09:49 +0200

    Add sfx of box moving

--- Assets/src/scripts/box/Box.cs
+++ Assets/src/scripts/box/Box.cs
@@ -5,6 +5,7 @@ public class Box : MonoBehaviour {
 	private SpriteRenderer sr;
 	private Color defaultColor;
 	private Color onTargetColor;
+	private AudioSource boxMovingSfx;
 
 	private void Start() {
 		GameObject levelManager = GameObject.Find("level_manager");
@@ -13,9 +14,13 @@ public class Box : MonoBehaviour {
 		this.sr = this.GetComponent<SpriteRenderer>();
 		this.defaultColor = this.sr.color;
 		this.onTargetColor = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+
+		this.boxMovingSfx = this.GetComponent<AudioSource>();
 	}
 
 	public void OnPositionChanged() {
+		this.boxMovingSfx.Play();
+
 		GridNode<Cell> nodeBelow = this.grid.FindNodeByPosition(this.transform.position);
 		this.sr.color = nodeBelow.GetValue().GetCellType() == CellType.TARGET ? this.onTargetColor : this.defaultColor;
 	}
