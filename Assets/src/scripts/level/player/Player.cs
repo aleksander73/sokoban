f69ab369f1052673011a85310b4d619d29124cd0
aleksander73 <olekdobkowski@gmail.com>
2025-06-08 21:56:14 +0200

    Restructure project

--- Assets/src/scripts/level/player/Player.cs
+++ Assets/src/scripts/level/player/Player.cs
@@ -160,42 +160,4 @@ public class Player : MonoBehaviour {
 	public void SetWIndowEnabled(bool windowEnabled) {
 		this.windowEnabled = windowEnabled;
 	}
-
-	// ==================================================
-
-	public class GameObjectAnimation<T> {
-		private readonly GameObject gameObject;
-		private readonly Animation<T> animation;
-		private readonly Action<T, GameObject> applyResult;
-		public Action<GameObject> onStarted;
-		public Action<GameObject> onFinished;
-
-		public GameObjectAnimation(GameObject gameObject, Animation<T> animation, Action<T, GameObject> applyResult) {
-			this.gameObject = gameObject;
-			this.animation = animation;
-			this.applyResult = applyResult;
-		}
-
-		public void Start() {
-			this.animation.Start();
-            this.onStarted?.Invoke(this.gameObject);
-        }
-
-		public void Update() {
-			if(this.IsFinished()) {
-				return;
-			}
-
-			T result = this.animation.Update();
-			this.applyResult(result, this.gameObject);
-
-			if(this.animation.IsFinished()) {
-				this.onFinished?.Invoke(this.gameObject);
-			}
-		}
-
-		public bool IsFinished() {
-			return this.animation.IsFinished();
-		}
-	}
 }
